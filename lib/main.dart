3f7a77754a9769e715ba39d63be45fadd25ddf29
project_dev <aquaspace0803@gmail.com>
2023-10-23 01:25:46 +0900

    ファイル別の再生に対応

--- lib/main.dart
+++ lib/main.dart
@@ -33,13 +33,26 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  // audioplayers
   final audioplayers.AudioPlayer _audioPlayer = audioplayers.AudioPlayer();
-  audioplayers.AssetSource? _audioPlayerSource;
+  final Map<String, audioplayers.AssetSource> _audioPlayerSourceMap = {};
 
+  // just_audio
   final just_audio.AudioPlayer _justAudioPlayer = just_audio.AudioPlayer();
+  String _justAudioAssetName = "";
 
+  // 再生するフォーマット
+  static const _formatList = [
+    'MP3',
+    'M4A',
+    'OGG',
+    'WAV'
+  ];
+
+  // アプリケーション名
   static const String _applicationName = "AudioPlayerSample";
 
+  // 初期化済みフラグ
   bool _isInitialize = false;
 
   @override
@@ -47,12 +60,15 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
   }
 
+  // 初期化
   void _initialize() async{
     try{
       developer.log("audioplayers initialize start.", name: _applicationName);
       await _audioPlayer.setVolume(1.0);
       await _audioPlayer.setReleaseMode(audioplayers.ReleaseMode.loop);
-      _audioPlayerSource = audioplayers.AssetSource('SampleBGM.mp3');
+      for(var format in _formatList){
+        _audioPlayerSourceMap[format] = audioplayers.AssetSource('SampleBGM.${format.toLowerCase()}');
+      }
       developer.log("audioplayers initialize end.", name: _applicationName);
     }catch(e){
       developer.log("$e", name: _applicationName);
@@ -63,7 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
       developer.log("just_audio initialize start.", name: _applicationName);
       await _justAudioPlayer.setVolume(1.0);
       await _justAudioPlayer.setLoopMode(just_audio.LoopMode.all);
-      await _justAudioPlayer.setAsset('assets/SampleBGM.mp3');
+      //await _justAudioPlayer.setAsset('assets/SampleBGM.mp3');
       developer.log("just_audio initialize end.", name: _applicationName);
     }catch(e){
       developer.log("$e", name: _applicationName);
@@ -112,27 +128,29 @@ class _MyHomePageState extends State<MyHomePage> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      TextButton.icon(
-                        onPressed: !_isInitialize ? null :  (){
-                          _audioPlayer.play(_audioPlayerSource!);
-                        },
-                        icon: const Icon(Icons.play_circle, size: 64),
-                        label: const Text(
-                          'Play',
-                          style: TextStyle(fontSize: 64)
+                      for (var value in _formatList) ...{
+                        TextButton.icon(
+                          onPressed: !_isInitialize ? null :  (){
+                            _audioPlayer.play(_audioPlayerSourceMap[value]!);
+                          },
+                          icon: const Icon(Icons.play_circle, size: 24),
+                          label: Text(
+                              value,
+                              style: const TextStyle(fontSize: 24)
+                          ),
                         ),
-                      ),
-                      TextButton.icon(
-                        onPressed: !_isInitialize ? null :  (){
-                          _audioPlayer.stop();
-                        },
-                        icon: const Icon(Icons.stop_circle, size: 64),
-                        label: const Text(
-                          'Stop',
-                          style: TextStyle(fontSize: 64)
-                        )
-                      ),
+                      },
                     ]
+                  ),
+                  TextButton.icon(
+                      onPressed: !_isInitialize ? null :  (){
+                        _audioPlayer.stop();
+                      },
+                      icon: const Icon(Icons.stop_circle, size: 24),
+                      label: const Text(
+                          'Stop',
+                          style: TextStyle(fontSize: 24)
+                      )
                   ),
                 ],
               ),
@@ -154,27 +172,43 @@ class _MyHomePageState extends State<MyHomePage> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      TextButton.icon(
-                        onPressed: !_isInitialize ? null :  (){
-                          _justAudioPlayer.play();
-                        },
-                        icon: const Icon(Icons.play_circle, size: 64),
-                        label: const Text(
-                          'Play',
-                          style: TextStyle(fontSize: 64)
-                        )
-                      ),
-                      TextButton.icon(
-                        onPressed: !_isInitialize ? null :  (){
-                          _justAudioPlayer.stop();
-                        },
-                        icon: const Icon(Icons.stop_circle, size: 64),
-                        label: const Text(
-                          'Stop',
-                          style: TextStyle(fontSize: 64)
-                        )
-                      ),
+                      for (var value in _formatList) ...{
+                        TextButton.icon(
+                            onPressed: !_isInitialize ? null : () {
+                              String assetName = 'assets/SampleBGM.${value.toLowerCase()}';
+                              if(_justAudioAssetName == assetName){
+                                 //この場合、stop()メソッドで停止した位置からの再生になる。
+                                 _justAudioPlayer.play();
+
+                                 // 戦闘から再生したい場合
+                                 //_justAudioPlayer.seek(const Duration(seconds: 0, minutes: 0, hours: 0)).then((value) => _justAudioPlayer.play());
+                                 //_justAudioPlayer.seek(Duration.zero).then((value) => _justAudioPlayer.play());
+
+                              }else{
+                                _justAudioPlayer.setAsset(assetName).then((value){
+                                  _justAudioAssetName = assetName;
+                                  _justAudioPlayer.play();
+                                });
+                              }
+                            },
+                            icon: const Icon(Icons.play_circle, size: 24),
+                            label: Text(
+                                value,
+                                style: const TextStyle(fontSize: 24)
+                            )
+                        ),
+                      },
                     ]
+                  ),
+                  TextButton.icon(
+                      onPressed: !_isInitialize ? null :  (){
+                        _justAudioPlayer.stop();
+                      },
+                      icon: const Icon(Icons.stop_circle, size: 24),
+                      label: const Text(
+                          'Stop',
+                          style: TextStyle(fontSize: 24)
+                      )
                   ),
                 ]
               )
